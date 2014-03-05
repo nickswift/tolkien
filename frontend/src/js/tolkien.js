15ce9d291d4c9e9eb007297a3210dc43d6d537de
Nick Wood <nickswift498@gmail.com>
2014-03-05 12:53:31 -0800

    testing csrf

--- frontend/src/js/tolkien.js
+++ frontend/src/js/tolkien.js
@@ -6,13 +6,19 @@ angular.module('tolkienApp', [
         'ngRoute',
         'tolkienControllers'
     ])
-    .config(['$routeProvider', function($routeProvider){
-        $routeProvider
-            .when('/', {
-                templateUrl: 'res/views/password.html',
-                controller : 'PasswdCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    }]);
+    .config(['$routeProvider', '$httpProvider',
+        function($routeProvider, $httpProvider){
+            /*
+            $httpProvider.defaults.useXDomain = true;
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+            */
+            
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'res/views/password.html',
+                    controller : 'PasswdCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        }]);
