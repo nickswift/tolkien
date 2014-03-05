2301dc7a15467d74b2a2ecb2af4af4474b762ea4
Nick Wood <nickswift498@gmail.com>
2014-03-05 10:58:13 -0800

    permissions and settings altered

--- tolkien/settings.py
+++ tolkien/settings.py
@@ -56,10 +56,19 @@ WSGI_APPLICATION = 'tolkien.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgres'
+    }
+}
+
+'''
+# TODO: get postgres working and remove this
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
